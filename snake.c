ecb8a85e6eedd32fcfb0bf827efb429a91dbc78d
MasterJohnsonTV <rrmasterbusiness@gmail.com>
2026-08-26 17:27:46 +0200

    update

--- snake.c
+++ snake.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// TODO: implement the rest os WSAD (SAD)
+void clear_screen() {
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
+}
+
+// TODO: Clear the input after getting the input
+// TODO: Add a dot map
 int main(void) {
   char input;
   printf("Welcome to snake game!\n");
@@ -8,13 +18,37 @@ int main(void) {
   printf("@\n");
 
   printf("WSAD: ");
-  scanf("%c", &input);
-  switch (input) {
-  case 'A':
-  case 'a':
-    printf("@.");
-    break;
-  default:
-    printf("wrong option");
+
+  while (1) {
+    scanf(" %c", &input);
+    switch (input) {
+    case 'W':
+    case 'w':
+      clear_screen();
+      printf("@\n");
+      printf(".");
+      break;
+    case 'S':
+    case 's':
+      clear_screen();
+
+      printf(".\n");
+      printf("@");
+      break;
+    case 'A':
+    case 'a':
+      clear_screen();
+
+      printf("@.");
+      break;
+    case 'D':
+    case 'd':
+      clear_screen();
+
+      printf(".@");
+      break;
+    default:
+      printf("wrong option");
+    }
   }
 }
